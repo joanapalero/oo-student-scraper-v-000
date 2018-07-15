25d006c13d43a459be65560b90a7cae553ca7292
Joana Palero <joana.palero24@gmail.com>
2018-07-15 14:52:49 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,7 +17,27 @@ end
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     profile = {}
-    
+    profile[:bio] = doc.css(".description-holder")[0].text.strip
+    profile[:profile_quote] = doc.css(".profile-quote").text
+    counter = 1
+    media = doc.css(".social-icon-container")[0].children
+    while(counter < media.length)
+      sm = media[counter].attributes["href"].value
+      case sm.gsub(/https:\/\/|www./, "").split(".")[0]
+        when "twitter"
+          profile[:twitter] = sm
+        when "linkedin"
+          profile[:linkedin] = sm
+        when "github"
+          profile[:github] = sm
+        when "youtube"
+          profile[:youtube] = sm
+        else
+          profile[:blog] = sm
+      end
+      counter += 2
+    end
+    profile
   end
 
 end

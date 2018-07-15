a770019543b41fa4d6d3775d6423c2d427fb1250
Joana Palero <joana.palero24@gmail.com>
2018-07-15 14:52:07 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,15 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    doc.css(".student-card").map do |student|
+      {
+        :name => student.css(".student-name").text,
+        :location => student.css(".student-location").text,
+        :profile_url => student.children[1].attributes["href"].value 
+      }
   end
+end
 
   def self.scrape_profile_page(profile_url)
     
